e4232e4c51bcdc7ef963c6944d30a937b0699071
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2024-03-20 04:10:41 +0500

    Add files via upload

---
+++ Javascript/JS Null &  Undefined Values/index.js
@@ -0,0 +1,8 @@
+// Javascript null & undefined values.
+
+let myVar
+//console.log(myVar) => console value is undefined because not assign a value in variable! 
+
+myVar = null
+// console.log(myVar) => console value is null because i am comparing to null value!
+
