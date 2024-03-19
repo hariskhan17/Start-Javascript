08b31a17b83e1aa5202eceb84168311941aedffe
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2024-03-20 04:12:09 +0500

    Add files via upload

---
+++ Javascript/JS Functions/index.js
@@ -0,0 +1,43 @@
+// Javascript Functions!
+
+// function exm() {
+//     let num1 = 10
+//     let num2 = 800
+//     let sumNum = num1 + num2
+//     console.log(sumNum)
+// }
+// exm()
+
+// return!
+
+// function exm2() {
+//     let num = 5
+//     let num7 = 5
+//     let sum = num +num7
+//     return sum
+// }
+// console.log(exm2())
+
+// arguments!
+
+// let userInfo =+prompt('Enter Any Num1')
+// let userInfo1 =+prompt('Enter Any Num2')
+
+// function arg(userInfo , userInfo1) {
+//     let sum = userInfo + userInfo1
+//     console.log(sum)
+// }
+
+// arg(userInfo,userInfo1)
+
+
+// let userInfo =+prompt('Enter Any Num1')
+// let userInfo1 =+prompt('Enter Any Num2')
+
+// function eXp(userInfo , userInfo1) {
+//     let multiply= userInfo * userInfo1
+//     console.log(multiply)
+// }
+
+// eXp(userInfo,userInfo1)
+
