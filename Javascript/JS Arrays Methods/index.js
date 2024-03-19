686357368def063be738bb7d620180e302d1b5aa
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2024-03-20 04:08:31 +0500

    Add files via upload

---
+++ Javascript/JS Arrays Methods/index.js
@@ -0,0 +1,28 @@
+// javascript Arrays-Methods!
+
+// push =>  The push() method adds new items to the end of an array.
+
+// let carsName = ['Audi','Mercedes','Civic','Sonata','Fortuner','Revo','Landcruiser-v8']
+// carsName.push('Corolla')
+// console.log(carsName)
+
+// pop => The pop() method of Array instances removes the last element from an array.
+
+// carsName.pop()
+// console.log(carsName)
+
+// splice => splice() is a JavaScript Array method that is used to remove, replace or insert items starting at a chosen index.
+
+// 1st tells where to start.
+// 2nd tells how many items to be deleted.
+// 3rd and 4th and so on tells what items to be added.
+// carsName.splice(2,0, 'Alto' )
+// console.log(carsName)
+
+// concat => concat() method concatenates (joins) two or more arrays.
+
+// let a = ['Haris', 'Khan']
+// let b = ['Habib','Khan']
+// let c = a.concat(b)
+// console.log(c) 
+
