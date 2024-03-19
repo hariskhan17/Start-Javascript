0b6898d2ac068e66f5a6a57647581a738b297f51
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2024-03-20 04:08:00 +0500

    Add files via upload

---
+++ Javascript/JS Arrays/index.js
@@ -0,0 +1,13 @@
+// Javascript Arrays!
+
+// Arrays[] => An array in JavaScript is a type of global object that is used to store data. Arrays consist of an ordered collection or list containing zero or more data types, and use numbered indices starting from 0 to access specific items.
+
+let carsName = ['Audi','Mercedes','Civic','Sonata','Fortuner','Revo','Landcruiser-v8']
+console.log(carsName)
+console.log(typeof(carsName)) // Arrays type is "Object"
+console.log(carsName[2])
+
+//Update a value!
+carsName[1] = 'Swift'
+console.log(carsName)
+
