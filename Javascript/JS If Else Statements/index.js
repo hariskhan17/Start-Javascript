bebf176f63fc6e1690ec10098e023cad9373b6ec
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2024-03-20 04:09:05 +0500

    Add files via upload

---
+++ Javascript/JS If Else Statements/index.js
@@ -0,0 +1,24 @@
+// Javascript (IF / ELSE) & (Else / If) Statements!
+
+// Examples Of If Else Statements
+
+// if (5 > 8) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// if ('haris' == 'Haris') {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// Example 
+if (2 > 5) {
+    console.log(true)
+}else if(6 > 5){
+console.log(true)
+}else{
+console.log(false)
+}
