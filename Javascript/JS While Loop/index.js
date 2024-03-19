8e5f1a7d3f9111b88b64f91fcadf12d0ae45d63b
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2024-03-20 04:15:58 +0500

    Add files via upload

---
+++ Javascript/JS While Loop/index.js
@@ -0,0 +1,10 @@
+// Javascript While-Loop!
+
+// while-loop => The while statement creates a loop (araund a code block) that is executed while a condition is true . The loop runs while the condition is true . Otherwise it stops.
+
+let num = 1
+while (num <= 10) {
+document.write(num + ') Hello World <br/>')
+num++
+}
+// console.log('war gaya')
