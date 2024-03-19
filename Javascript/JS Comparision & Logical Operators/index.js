a7ff5fd843ef15510ddccb58dddfd7f2a9b162fe
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2024-03-20 04:13:44 +0500

    Add files via upload

---
+++ Javascript/JS Comparision & Logical Operators/index.js
@@ -0,0 +1,35 @@
+// Javascript Comparision-Operators!
+
+// == => == in JavaScript is used for comparing two variables, but it ignores the datatype of variable. 
+
+// if (5 == 4) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// === => === is used for comparing two variables, but this operator also checks datatype and compares two values. 
+
+// if (10 === '10') {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// Javascript Logical-Operator!
+
+// && => The AND operator returns true if both conditions are true.
+
+// if (15> 10 && 4 < 6) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// || => the OR operator returns true if one of the conditions is true.
+
+// if (3 > 8 ||  9 < 2) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
