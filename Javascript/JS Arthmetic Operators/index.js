15f29249b624426ffb9b5499e36dfc2b5f555d03
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2024-03-20 04:13:07 +0500

    Add files via upload

---
+++ Javascript/JS Arthmetic Operators/index.js
@@ -0,0 +1,19 @@
+// Javascript Arthmetic Operators!
+
+let num1 = 10;
+let num2 = 15;
+let num3 = 20;
+
+// (1) => Addition.
+// console.log(num1 + num2)
+
+// (2) => Subtraction.
+// console.log(num3 - num1)
+
+// (3) => Multipilication.
+// console.log(num1 * num2)
+
+// (4) => Division.
+// console.log(num2 / num3)
+
+// (5) => Modulus.
