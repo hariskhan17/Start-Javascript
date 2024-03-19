93249c27645f906aa130df96ea7f61e80a2410b7
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2024-03-20 04:15:25 +0500

    Add files via upload

---
+++ Javascript/JS For Loop/index.js
@@ -0,0 +1,14 @@
+// Javascript For-Loop!
+
+// ForLoop => 
+
+// let arr = ['Hello' , 'Haris', 'Habib','Khan']
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+    
+// }
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(i  + arr);
+    
+// }
